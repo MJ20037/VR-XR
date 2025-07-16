a5af369f9ccd8f683428940a5688eb596053ada5
MJ20037 <mriduljoshi836@gmail.com>
2025-07-16 20:32:58 +0530

    Added hand position limit

--- Assets/Scripts/SimpleHingeINteractable.cs
+++ Assets/Scripts/SimpleHingeINteractable.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class SimpleHingeInteractable : XRSimpleInteractable
 {
+    [SerializeField] Vector3 positionLimits;
     private Transform grabHand;
+    private Collider hingeCollider;
+    private Vector3 hingePositions;
     private const string Default_Layer = "Default";
     private const string Grab_Layer = "Grab";
     [SerializeField] bool isLocked;
+
+    protected virtual void Start()
+    {
+        hingeCollider = GetComponent<Collider>();
+    }
 
     public void UnlockHinge()
     {
@@ -22,7 +32,7 @@ public class SimpleHingeInteractable : XRSimpleInteractable
     {
         if (grabHand != null)
         {
-            transform.LookAt(grabHand, transform.forward);
+            TrackHand();
         }
     }
 
@@ -39,6 +49,27 @@ public class SimpleHingeInteractable : XRSimpleInteractable
         base.OnSelectExited(args);
         grabHand = null;
         ChangeLayerMask(Grab_Layer);
+    }
+
+    private void TrackHand()
+    {
+        transform.LookAt(grabHand, transform.forward);
+        hingePositions = hingeCollider.bounds.center;
+        if (grabHand.position.x >= hingePositions.x + positionLimits.x ||
+        grabHand.position.x<=hingePositions.x-positionLimits.x)
+        {
+            ReleaseHinge();
+        }
+        else if (grabHand.position.y >= hingePositions.y + positionLimits.y ||
+        grabHand.position.y<=hingePositions.y-positionLimits.y)
+        {
+            ReleaseHinge();
+        }
+        else if (grabHand.position.z >= hingePositions.z + positionLimits.z ||
+        grabHand.position.z <= hingePositions.z - positionLimits.z)
+        {
+            ReleaseHinge();
+        }
     }
 
     public void ReleaseHinge()
